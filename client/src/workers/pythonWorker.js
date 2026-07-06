4127ff626ed1cdea74106556519930408c1dba4c
Nikita <nikitamulam2005@gmail.com>
2026-07-06 21:56:34 +0530

    fixed the python worker

--- client/src/workers/pythonWorker.js
+++ client/src/workers/pythonWorker.js
@@ -51,7 +51,7 @@ self.onmessage = async function (e) {
       const stdinLines = stdin
         .split(/\r?\n/)
         .map((line) => line.trimEnd());
-      
+
       let stdinIndex = 0;
 
       pyodide.registerJsModule("stdin_helper", {
@@ -61,12 +61,17 @@ self.onmessage = async function (e) {
           }
           return "";
         },
+        at_end: () => stdinIndex >= stdinLines.length,
       });
 
       // Override builtins.input in Python to read from our stdin helper
       // It prints the prompt and the entered value to simulate console output.
+      // Also back sys.stdin with the same stdin_helper data so scripts using
+      // sys.stdin.read()/.readline()/iteration work too.
       await pyodide.runPythonAsync(`
 import builtins
+import sys
+import io
 import stdin_helper
 
 def custom_input(prompt=""):
@@ -77,6 +82,33 @@ def custom_input(prompt=""):
     return val
 
 builtins.input = custom_input
+
+# --- sys.stdin backed by the same stdin_helper data ---
+class _FakeStdin(io.TextIOBase):
+    def readline(self, size=-1):
+        line = stdin_helper.readline()
+        if line == "" and stdin_helper.at_end():
+            return ""
+        return line + "\\n"
+
+    def read(self, size=-1):
+        lines = []
+        while True:
+            if stdin_helper.at_end():
+                break
+            lines.append(stdin_helper.readline())
+        return "\\n".join(lines) + ("\\n" if lines else "")
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.readline()
+        if line == "":
+            raise StopIteration
+        return line
+
+sys.stdin = _FakeStdin()
       `);
 
       // Load packages imported in the user code (e.g. numpy, pandas, matplotlib)
